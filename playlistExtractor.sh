b1a10233690acc2db3fa3f7d93c2dbbb3e062d94
bbssamuray <bbssamuraymi@gmail.com>
2022-09-25 20:02:47 +0300

    Various enhancements

--- playlistExtractor.sh
+++ playlistExtractor.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-#SELECT title FROM streams WHERE uid IN (SELECT stream_id FROM playlist_stream_join WHERE playlist_id == 3);
 
 set -o pipefail -e
 #Exit script if there are any errors
- 
-outFileName="output.txt"
 
-outputFile(){
+tempDB=""
+playlistData=()
+override=""
+sourceName=""
 
-	tempDB=""
-	sourceName=$1
+getPlaylistData(){
+	
 	#Name of .db or .zip file.
 	
 	if [[ -z $sourceName ]]
 	then
 		sourceName="newpipe.db"
+		#newpipe.db will be default if no input file is given
 	fi
 
 	if [[ ! -f "$sourceName" ]]
@@ -26,14 +27,9 @@ outputFile(){
 
 	if [[ "$sourceName" == *.zip ]]
 	then
-		if test -f "temp.db"
-		then
-    			echo "temp.db already exists, please delete it before proceeding."
-    			exit 1
-		fi
 
 		echo "Getting newpipe.db from $sourceName."
-		tempDB="temp.db"
+		tempDB=$(mktemp)
 		unzip -p "$sourceName" newpipe.db > "$tempDB" || (rm temp.db && exit 1)
 		sourceName=$tempDB
 
@@ -42,57 +38,77 @@ outputFile(){
 		echo "Only .db and .zip files are supported."
 		exit 1
 	fi
-
-
-	sqlite3 $sourceName "SELECT uid,name FROM playlists;" | sort -g
-	read -p "Enter the id of playlist you want to extract:" playlistID
-	sqlite3 $sourceName "SELECT url FROM streams WHERE uid IN (SELECT stream_id FROM playlist_stream_join WHERE playlist_id == $playlistID);" > "$outFileName"
-
-	if [[ ! -z $tempDB ]]
-	then
-		echo "Deleting temporary database..."
-		rm $tempDB
-	fi
-
+	
+	#Put all of the playlists into an array
+	#Requires bash 4.4 or newer
+	mapfile -t playlistData < <(sqlite3 "$sourceName" "SELECT uid,name FROM playlists;" | sort -g)
 
 }
 
-helpMessage()
-{
+outputPlaylistFile(){
+
+	sqlite3 "$sourceName" "SELECT url || \" #\" || title FROM streams WHERE uid IN (SELECT stream_id FROM playlist_stream_join WHERE playlist_id == $1);" > "$2".txt
+
+}
+
+
+helpMessage(){
+
 	# Display Help
 	echo "Extracts urls from Newpipe playlists."
 	echo "Accepts either the .zip obtained from the \"export database\" function in the settings menu or the newpipe.db inside of the zip."
 	echo
-	echo "Usage: playlistExtractor [-o fileName] [database|zip name]"
+	echo "Usage: playlistExtractor [-o] [database|zip name]"
 	echo
 	echo "options:"
 	echo "-h    Print this help message."
-	echo "-o    Output file. output.txt by default"
+	echo "-o    Overrides files instead of skipping"
    	echo
 }
 
 main(){
         
-	while getopts ":ho:" option; do
+	while getopts ":ho" option; do
    		case $option in
 			h) #Display help message
-        			helpMessage
-	        		exit 0;;
-      			o) #Set output file
-				echo $OPTARG 123
-	        		outFileName=$OPTARG
-				shift 2;;
+        		helpMessage
+	        	exit 0;;
+      		o) #override
+				override="true"
+				shift 1;;
 			\?) # Invalid option
 				echo "Error: Invalid option"
-         			exit 0;;
+         		exit 1;;
    		esac
-	done        
+	done
+	if [[ "" || ! -f "newpipe.db" ]]     
+	then
+		echo "thats true"
+	fi
+
+	getPlaylistData "$1"
+
+	for (( i=0; i<${#playlistData[@]}; i++ ))
+	do
+		id=$(echo "${playlistData[$i]}" | cut -d "|" -f 1)
+		name=$(echo "${playlistData[$i]}" | cut -d "|" -f 2-)
+		echo "Getting $name..."
+		if [[ "$override" || ! -f "$name.txt" ]]
+		then
+			sqlite3 "$sourceName" "SELECT url || \" #\" || title FROM streams WHERE uid IN (SELECT stream_id FROM playlist_stream_join WHERE playlist_id == $id);" > "$name".txt
+		else
+			echo "$name.txt already exists, skipping."
+		fi
+	done
+		
 	
-	outputFile $1
+	if [[ -n $tempDB ]]
+	then
+		echo "Deleting temporary database..."
+		rm "$tempDB"
+	fi
+
 }     
 
-main $@
-
-
-
+main "$@"
 
